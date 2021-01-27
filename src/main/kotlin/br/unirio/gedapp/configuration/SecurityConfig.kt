9ba4cd5ad2492dfbf69e23f3854616d1e2c3edfa
Davi Faria <d.a.faria@gmail.com>
2021-01-27 19:30:24 -0300

    setting cors configrations to allow front and backend communication

--- src/main/kotlin/br/unirio/gedapp/configuration/SecurityConfig.kt
+++ src/main/kotlin/br/unirio/gedapp/configuration/SecurityConfig.kt
@@ -1,19 +1,19 @@
 package br.unirio.gedapp.configuration
 
 import br.unirio.gedapp.service.UserService
-import org.springframework.security.config.http.SessionCreationPolicy
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-
+import com.google.common.collect.ImmutableList
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -32,5 +32,18 @@ class SecurityConfig(private val userSvc: UserService) : WebSecurityConfigurerAd
             .and()
             .csrf().disable()
             .headers().frameOptions().disable()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = ImmutableList.of("*")
+        configuration.allowedMethods = ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+        configuration.allowCredentials = true
+        configuration.allowedHeaders = ImmutableList.of("Authorization", "Cache-Control", "Content-Type")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
