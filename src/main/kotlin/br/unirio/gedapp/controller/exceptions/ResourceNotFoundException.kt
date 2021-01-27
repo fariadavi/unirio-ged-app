d608c49964ff3745394741b0535a0d1f1f719f03
Davi Faria <d.a.faria@gmail.com>
2021-01-27 03:51:47 -0300

    add missing exception

---
+++ src/main/kotlin/br/unirio/gedapp/controller/exceptions/ResourceNotFoundException.kt
@@ -0,0 +1,7 @@
+package br.unirio.gedapp.controller.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class ResourceNotFoundException(message: String = "Resource Not Found") : RuntimeException(message) {}
