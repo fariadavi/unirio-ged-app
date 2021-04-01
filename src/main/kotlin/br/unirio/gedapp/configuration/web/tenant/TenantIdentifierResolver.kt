ca1c57e8156c2a45adb7fb1bcef6471a7ea1b1be
Davi Faria <d.a.faria@gmail.com>
2021-04-01 19:55:44 -0300

    Adjust current tenant resolver logic to get from authenticated user's department and document controller way of retrieving current tenant

--- src/main/kotlin/br/unirio/gedapp/configuration/web/tenant/TenantIdentifierResolver.kt
+++ src/main/kotlin/br/unirio/gedapp/configuration/web/tenant/TenantIdentifierResolver.kt
@@ -1,13 +1,10 @@
 package br.unirio.gedapp.configuration.web.tenant
 
-import java.security.Principal
-
+import br.unirio.gedapp.domain.User
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver
 import org.springframework.security.authentication.AnonymousAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
-import java.util.*
-import java.util.function.Predicate
 
 @Component
 class TenantIdentifierResolver : CurrentTenantIdentifierResolver {
@@ -16,12 +13,13 @@ class TenantIdentifierResolver : CurrentTenantIdentifierResolver {
         const val DEFAULT_TENANT = "public"
     }
 
-    override fun resolveCurrentTenantIdentifier(): String {
-        return Optional.ofNullable(SecurityContextHolder.getContext().authentication)
-            .filter(Predicate.not { authentication -> authentication is AnonymousAuthenticationToken })
-            .map { obj: Principal -> obj.name }
-            .orElse(DEFAULT_TENANT)
-    }
+    override fun resolveCurrentTenantIdentifier() = (
+            SecurityContextHolder.getContext().authentication
+                ?.takeUnless { it is AnonymousAuthenticationToken }?.principal
+                ?.takeIf { it is User }
+                ?.let { it as User }
+            )?.currentDepartment?.acronym
+        ?: DEFAULT_TENANT
 
     override fun validateExistingCurrentSessions(): Boolean = true
 }
--- src/main/kotlin/br/unirio/gedapp/controller/DocumentController.kt
+++ src/main/kotlin/br/unirio/gedapp/controller/DocumentController.kt
@@ -1,15 +1,17 @@
 package br.unirio.gedapp.controller
 
+import br.unirio.gedapp.configuration.web.tenant.TenantIdentifierResolver
 import br.unirio.gedapp.domain.Document
-import br.unirio.gedapp.domain.User
 import br.unirio.gedapp.repository.DocumentRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/documents")
-class DocumentController(@Autowired private val docRepo: DocumentRepository) {
+class DocumentController(
+    @Autowired private val docRepo: DocumentRepository,
+    @Autowired private val tenantResolver: TenantIdentifierResolver
+) {
 
     @GetMapping("/{id}")
     fun getDocument(@PathVariable id: String): Document = docRepo.findById(id).get()
@@ -21,11 +23,11 @@ class DocumentController(@Autowired private val docRepo: DocumentRepository) {
     fun addDocument(@RequestBody doc: Document): Document = docRepo.save(doc)
 
     @DeleteMapping("/{id}")
-    fun deleteDocument(@PathVariable id : String) = docRepo.deleteById(id)
+    fun deleteDocument(@PathVariable id: String) = docRepo.deleteById(id)
 
     @GetMapping("/search")
-    fun searchDocuments(@RequestParam q: String): Iterable<Document> {
-        val user = SecurityContextHolder.getContext().authentication.principal as User
-        return docRepo.findByTenantAndContentMatches(user.department!!.acronym, q)
-    }
+    fun searchDocuments(@RequestParam q: String) =
+        docRepo.findByTenantAndContentMatches(
+            tenantResolver.resolveCurrentTenantIdentifier(),
+            q)
 }
