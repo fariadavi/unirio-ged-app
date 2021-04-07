9e9272bd1490e2c6f95c1968e7d8812e212b4151
Davi Faria <d.a.faria@gmail.com>
2021-04-07 18:28:27 -0300

    Fixed category table creation script fk column name to match other tables pattern

--- src/main/resources/db/migration/tenants/V1__init_tenant_schema.sql
+++ src/main/resources/db/migration/tenants/V1__init_tenant_schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE category (
     id			BIGINT GENERATED ALWAYS AS IDENTITY,
     name       	VARCHAR(40) NOT NULL,
-    parent      BIGINT,
+    parent_id      BIGINT,
     PRIMARY KEY  (id),
     CONSTRAINT fk_parent
-        FOREIGN KEY(parent)
+        FOREIGN KEY(parent_id)
             REFERENCES category(id)
 );
 
