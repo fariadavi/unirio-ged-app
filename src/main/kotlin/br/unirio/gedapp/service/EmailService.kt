ef18b84a2793c0eac883f5ce6c10f6c9c21a23dc
Davi Faria <d.a.faria@gmail.com>
2023-09-29 18:09:44 -0300

    fix load email template

--- src/main/kotlin/br/unirio/gedapp/service/EmailService.kt
+++ src/main/kotlin/br/unirio/gedapp/service/EmailService.kt
@@ -4,6 +4,7 @@ import br.unirio.gedapp.domain.Department
 import br.unirio.gedapp.domain.User
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import javax.mail.internet.MimeMessage
@@ -17,7 +18,7 @@ class EmailService(val javaMailSender: JavaMailSender) {
         msg.setRecipients(MimeMessage.RecipientType.TO, recipient)
         msg.subject = "UNIRIO GED App | Bem-vindo ao ${invitedDepartment.acronym}"
 
-        var htmlTemplate: String = Files.readString(Path.of("src/main/resources/email", "template.html"))
+        var htmlTemplate: String = this::class.java.getResource("/email/template.html")!!.readText()
 
         htmlTemplate = htmlTemplate.replace("\${invitingUserName}", invitingUser.fullName)
         htmlTemplate = htmlTemplate.replace("\${invitingUserEmail}", invitingUser.email)
